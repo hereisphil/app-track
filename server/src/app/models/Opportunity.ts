bacd4b62956a643be88fb21949be199849b2d5e6
hereisphil <thereisphil@gmail.com>
2026-01-12 16:37:45 -0600

    Made user required to add opp

--- server/src/app/models/Opportunity.ts
+++ server/src/app/models/Opportunity.ts
@@ -34,6 +34,7 @@ const oppSchema = new Schema(
         user: {
             type: Schema.Types.ObjectId,
             ref: "User",
+            required: [true, "Associated user is required"],
         },
     },
     { timestamps: true }
