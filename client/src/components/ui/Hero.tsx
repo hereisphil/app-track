3d9dbe2c1b9898590b17d419c3d3c856f6d24eb5
hereisphil <thereisphil@gmail.com>
2026-02-19 17:27:57 -0600

    Updated Hero's typography

--- client/src/components/ui/Hero.tsx
+++ client/src/components/ui/Hero.tsx
@@ -1,10 +1,10 @@
 const Hero = () => {
     return (
-        <section className="text-cyan-600 flex flex-col justify-center gap-4 max-w-lg text-center md:text-left">
-            <h2 className="text-4xl font-bold">
+        <section className="text-cyan-600 flex flex-col justify-center gap-4 md:gap-8 max-w-lg text-center md:text-left">
+            <h2 className="text-4xl md:text-6xl font-bold">
                 One Place To Track All Your Opportunities
             </h2>
-            <p>No more tables and rows</p>
+            <p className="text-lg md:text-2xl">No more tables and rows</p>
         </section>
     );
 };
